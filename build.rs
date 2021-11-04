ddd0df9e8d3ab71056cfc5e8ced240f0b44bcc05
Nikola Ilo <nikola.ilo@gmail.com>
2021-11-04 13:18:17 +0100

    pkg: embed windows manifest to support long file paths if enabled on system

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
+use clap::Shell;
 use std::env;
 use std::fs::{self, File};
 use std::io::{self, Read, Write};
 use std::path::Path;
 use std::process;
 
-use clap::Shell;
-
 use app::{RGArg, RGArgKind};
+extern crate embed_resource;
 
 #[allow(dead_code)]
 #[path = "crates/core/app.rs"]
@@ -48,6 +48,9 @@ fn main() {
     if let Some(rev) = git_revision_hash() {
         println!("cargo:rustc-env=RIPGREP_BUILD_GIT_HASH={}", rev);
     }
+
+    // embed windows application manifest file
+    embed_resource::compile("rg-manifest.rc");
 }
 
 fn git_revision_hash() -> Option<String> {
